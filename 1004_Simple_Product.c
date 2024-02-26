efafb681a84663a149a3ff4e52377cb2c6ccbf47
Sudipto Roy Shawon <sudipto.roy@northsouth.edu>
2024-02-26 20:18:31 +0600

    Create 1004_Simple_Product.c

---
+++ 1004_Simple_Product.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int a,b,PROD;
+    scanf("%d%d",&a,&b);
+    PROD=a*b;
+    printf("PROD = %d\n",PROD);
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
